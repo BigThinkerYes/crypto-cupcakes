ae1d70c08324235becc9d8c55a17ce0b4f46013a
BigThinkerYes <designaspnet4@hotmail.com>
2023-02-09 15:06:47 -0600

    cupcake data

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A Cupcake API using Auth0's OIDC for authentication and OAuth for authorization
 
     npm i
     npm run start-dev
+    npm install express express-openid-connect --save
+    In .env:
+    MY_SECRET = PUT_YOUR_OWN_SECRET_HERE
+    CLIENT_ID = Auth0_provided_ID
+    BASE_URL = http://localhost:3000
+    AUTH0_DOMAIN = Auth0_provided_domain
--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
 // TODO - require express-openid-connect and destructure auth from it
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_AUDIENCE,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_BASE_URL,
+};
+//
 const { User, Cupcake } = require('./db');
 
 // middleware
@@ -13,6 +24,8 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
@@ -20,6 +33,11 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
@@ -41,4 +59,3 @@ app.use((error, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
-
