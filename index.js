9807215eceb11cb04a3a2b7dbd8e51b2730795ec
BigThinkerYes <designaspnet4@hotmail.com>
2023-02-15 14:16:23 -0600

    new get cmd

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
-// TODO - require express-openid-connect and destructure auth from it
-const { auth } = require('express-openid-connect');
-require('dotenv').config();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_AUDIENCE,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_BASE_URL,
-};
 //
 const { User, Cupcake } = require('./db');
 
@@ -24,8 +13,6 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
@@ -33,10 +20,54 @@ app.use(auth(config));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+// TODO - require express-openid-connect and destructure auth from it
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
 
+const{
+  AUTH0_SECRET,
+  AUTH0_BASE_URL,
+  AUTH0_CLIENT_ID,
+  AUTH0_ISSUER_BASE_URL,
+} = process.env;
+
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: AUTH0_SECRET,
+  baseURL: AUTH0_BASE_URL,
+  clientID: AUTH0_CLIENT_ID,
+  issuerBaseURL: AUTH0_ISSUER_BASE_URL,
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+app.use(async (req, res, next) => {
+  try{
+    const [user] = await User.findOrCreate({
+      where: {
+        // properties mentioned above(username, name, email)
+        where: {name:user},
+        defaults:{
+          email:'email',
+          name:'username'
+        }
+      }
+    });
+    if (created) {
+      console.log(user.name);
+      res.send(user.name);
+      console.log(user.email);
+      res.send(user.email)
+    }
+  }
+  catch(error){
+    console.log(error);
+    next();
+  }
 });
 
 app.get('/cupcakes', async (req, res, next) => {
